6456f6dbf715846cc42ddd19c3a5fa61c6a80f3a
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2022-09-01 15:47:52 -0300

    2020-09-01

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import datesOfProyects from './components/dates_of_proyects.js';
-import scroll from "./components/scroll.js";
+
 addEventListener("DOMContentLoaded",() => {
     datesOfProyects();
-    scroll();
 });
